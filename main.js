96f4ea0e8195db0df1fb10f8cb923d55bbdc7449
Mark Dickinson <dickinsonmr1@gmail.com>
2025-10-25 07:11:45 -0400

    adding lil-gui and stats.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import GUI from 'lil-gui'; 
+import Stats from 'three/addons/libs/stats.module.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -7,6 +9,12 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
+
+const gui = new GUI();
+gui.add( document, 'title' );
+
+const stats = new Stats();
+document.body.appendChild(stats.dom)
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -21,5 +29,7 @@ function animate() {
   cube.rotation.y += 0.01;
 
   renderer.render( scene, camera );
+
+  stats.update();
 
 }
